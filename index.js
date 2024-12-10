2114b6d8d091f9db2b3fc4dad73d2d1bb8531b11
wakio-ngari <ngarijoyce017@gmail.com>
2024-12-10 22:05:15 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.getElementById('main');
+if (main) {
+  main.remove();
+}
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+newHeader.textContent = 'YOUR-NAME is the champion';
